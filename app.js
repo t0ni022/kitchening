8b296c0f86f6273cfb45524d7a6363701f6de572
Antonio <marco.a.falcon@outlook.com>
2023-11-29 15:34:43 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+/* require */
+const express = require('express');
+const path = require('path');
+
+/* settings */
+const app = express();
+const PORT = 3030;
+app.use(express.static(path.join(__dirname,'public')))
+
+
+/* routes */
+app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+
+
+/* server */
+app.listen(PORT, () => console.log(`server running in http://localhost:${PORT}`));
